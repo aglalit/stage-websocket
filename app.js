0405d5dd6d82c9727e5e0f03e69bd152e2fa7181
aglalit <marat.goya@gmail.com>
2017-01-31 01:31:24 +0500

    My changes

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ wss = new WebSocketServer({host:ipaddress, port:8080,
 });
 
 wss.on('connection', function(ws) {
-for (i=0;i<10;i++){
-  setTimeout(function(){
-    ws.send(Date.now())
-  })
-}
+// for (i=0;i<10;i++){
+//   setTimeout(function(){
+//     ws.send(Date.now())
+//   })
+// }
 
 var client_uuid = uuidV4();
 clients.push({"id": client_uuid, "ws": ws});
